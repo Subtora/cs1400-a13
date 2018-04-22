c349bfa0e37327330238662b3056b5d6fc7a9270
Trevor <16tsmith55@gmail.com>
2018-04-22 00:20:33 -0600

    experimenting with full-screen random polydraw

--- src/PaintPanel.java
+++ src/PaintPanel.java
@@ -9,7 +9,7 @@ public class PaintPanel extends JPanel implements ActionListener{
 
     public PaintPanel() {
         setSize(800,600);
-        Timer clock = new Timer(100,this);
+        Timer clock = new Timer(1,this);
         clock.start();
     }
 
@@ -18,18 +18,19 @@ public class PaintPanel extends JPanel implements ActionListener{
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g2d);
 
-        for (int n = 1; n<= 4; n++){
+        //for (int n = 1; n<= 4; n++){
 
-            int npoints = r(10,10);
+            int npoints = r(2000,2000);
             int[] xpoints = new int[npoints];
             int[] ypoints = new int[npoints];
 
             for (int i = 0; i < npoints;i++){
-                xpoints[i] = r((n*100),(n*100)+100);
-                ypoints[i] = r(100,200);
+                //xpoints[i] = r((n*100),(n*100)+100);
+                xpoints[i] = r(0,800);
+                ypoints[i] = r(0,600);
             }
             g.drawPolygon(xpoints, ypoints, npoints);
-        }
+        //}
     }//end paint
     public void actionPerformed(ActionEvent arg0) {
         repaint();
